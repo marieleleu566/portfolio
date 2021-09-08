78864396870bf49895348928a811bf7f9f84331f
Marie Leleu <marieleleu@icloud.com>
2021-09-09 01:15:31 +0200

    updated site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  get 'photography', to: 'pages#photography'
-  resources :contacts, only: [:new, :create], :path => 'contact'
+  get 'work', to: 'pages#work'
 end
